898824a25bad44676a2514214cc473f1810664fa
Jeyson Artiles <RA@Jeysons-Mac-Pro.local>
2015-06-03 06:34:18 -0400

    Second Revision
    Finally fixed; “fatal error: unexpectedly found nil while unwrapping an
    Optional value (lldb)”

--- ViewController.swift
+++ ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var headerLabel: UILabel!
     @IBOutlet weak var menShoeTextField: UITextField!
     @IBOutlet weak var womenShoeTextField: UITextField!
-    
     @IBOutlet weak var usSizeTitle: UILabel!
     @IBOutlet weak var euroSizeTitle: UILabel!
     @IBOutlet weak var usSizeValue: UILabel!
@@ -21,7 +20,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         usSizeTitle.hidden = true
         usSizeValue.hidden = true
         euroSizeTitle.hidden = true
@@ -30,15 +29,26 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-
     }
     
     @IBAction func convertButton(sender: UIButton) {
         
-        menShoeTextField.resignFirstResponder()
-        womenShoeTextField.resignFirstResponder()
-        
-        
+        if menShoeTextField.text == "" {
+            println("NIL")
+        } else {
+            headerLabel.textColor = UIColor.blackColor()
+            headerLabel.backgroundColor = UIColor.lightGrayColor()
+            menShoeTextField.resignFirstResponder()
+            var mensShoeTextFieldToInt = menShoeTextField.text.toInt()!
+            let menConversionConstant = 30
+            usSizeTitle.hidden = false
+            usSizeValue.hidden = false
+            usSizeValue.text = "\(mensShoeTextFieldToInt)"
+            euroSizeTitle.hidden = false
+            euroSizeValue.hidden = false
+            euroSizeValue.text = "\(mensShoeTextFieldToInt + menConversionConstant)"
+            menShoeTextField.text = ""
+        }
     }
 }
 
